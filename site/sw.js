7814f445cf849d2b554a36028fc47a052fe2788a
pianogame <bra.os.ost@gmail.com>
2026-09-27 06:53:58 +0900

    Refresh cache for new Dream Stage icon

--- site/sw.js
+++ site/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'piano-dream-stage-shell-v3';
+const CACHE_NAME = 'piano-dream-stage-shell-v4';
 const LEGACY_CACHE_PREFIX = 'piano-palette-shell-';
 const CACHE_PREFIX = 'piano-dream-stage-shell-';
 
